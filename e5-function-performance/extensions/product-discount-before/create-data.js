a4e57523e51b8873de97a9df844259e56d07feb5
Nick Wesselman <27013789+nickwesselman@users.noreply.github.com>
2023-12-21 15:50:17 -0500

    initial/inefficient function

---
+++ e5-function-performance/extensions/product-discount-before/create-data.js
@@ -0,0 +1,62 @@
+const sample = {
+    "merchandise": {
+        "__typename": "ProductVariant",
+        "id": "gid://shopify/ProductVariant/0",
+        "sku": "",
+        "title": null,
+        "weight": 5,
+        "requiresShipping": true,
+        "product": {
+            "hasTags": [
+                {
+                    "tag": "bronze",
+                    "hasTag": false
+                },
+                {
+                    "tag": "silver",
+                    "hasTag": false
+                },
+                {
+                    "tag": "gold",
+                    "hasTag": false
+                }
+            ],
+            "id": "gid://shopify/Product/7329301659670",
+            "title": "Creek Stone"
+        }
+    }
+}
+
+let input = {
+    "cart": {
+        "lines": [
+        ]
+    },
+    "discountNode": {
+        "metafield": {
+            "value": "[{\"tag\":\"bronze\",\"percentOff\":\"5\"},{\"tag\":\"silver\",\"percentOff\":\"10\"},{\"tag\":\"gold\",\"percentOff\":\"20\"}]"
+        }
+    }
+}
+
+for (let i = 0; i < 100; i++) {
+    const line = JSON.parse(JSON.stringify(sample));
+    line.merchandise.id = `gid://shopify/ProductVariant/000${i+1}`
+    line.merchandise.product.hasTags = [
+        {
+            "tag": "bronze",
+            "hasTag": i%5 == 0
+        },
+        {
+            "tag": "silver",
+            "hasTag": i%7 == 0
+        },
+        {
+            "tag": "gold",
+            "hasTag": i%17 == 0
+        }
+    ]
+    input.cart.lines.push(line);
+}
+
+console.log(JSON.stringify(input, null, 2));
---
+++ e5-function-performance/extensions/product-discount-before/src/run.js
@@ -0,0 +1,64 @@
+// @ts-check
+import { DiscountApplicationStrategy } from "../generated/api";
+
+/**
+ * @typedef {import("../generated/api").RunInput} RunInput
+ * @typedef {import("../generated/api").FunctionRunResult} FunctionRunResult
+ * @typedef {import("../generated/api").Discount} Discount
+ */
+
+/**
+ * @type {FunctionRunResult}
+ */
+const EMPTY_DISCOUNT = {
+  discountApplicationStrategy: DiscountApplicationStrategy.All,
+  discounts: [],
+};
+
+/**
+ * @param {RunInput} input
+ * @returns {FunctionRunResult}
+ */
+export function run(input) {
+  /**
+   * @type {{
+   *    tag: string
+   *    percentOff: string
+   * }[]}
+   */
+  const configuration = JSON.parse(
+    input?.discountNode?.metafield?.value ?? "{}"
+  );
+
+  console.error(JSON.stringify(configuration));
+
+  const lines = input.cart.lines;
+  const discounts = configuration.flatMap((config) => {
+    return lines.filter(
+        line => line.merchandise.__typename == "ProductVariant" &&
+        line.merchandise.product.hasTags.find(tag => tag.tag == config.tag && tag.hasTag)
+      ).map(line => /** @type {Discount} */ {
+        console.error(JSON.stringify(line));
+        return {
+          value: {
+            percentage: {
+              value: parseFloat(config.percentOff)
+            }
+          },
+          targets: [
+            {
+              productVariant: {
+                // @ts-ignore
+                id: line.merchandise.id
+              }
+            }
+          ],
+        };
+      });
+  });
+
+  return {
+    discountApplicationStrategy: DiscountApplicationStrategy.All,
+    discounts
+  };
+};
